da68c1cae83148d8ae78a7a3162d5d98d75f2997
Brady Six <bsixdev@gmail.com>
2024-10-24 20:18:08 -0700

    feat: add contact repository

---
+++ src/main/java/com/bsix/simplecontactapi/contact/ContactRepository.java
@@ -0,0 +1,5 @@
+package com.bsix.simplecontactapi.contact;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ContactRepository extends JpaRepository<Contact, String> {}
