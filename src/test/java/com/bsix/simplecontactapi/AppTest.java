9e507886c829cf65197e5c553ab5b50f7dc3b4f4
Brady Six <bsixdev@gmail.com>
2024-10-24 20:08:18 -0700

    init

---
+++ src/test/java/com/bsix/simplecontactapi/AppTest.java
@@ -0,0 +1,13 @@
+package com.bsix.simplecontactapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppTest {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
