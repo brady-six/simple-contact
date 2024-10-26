f3addbfe65022f8faaec9ced45410062c141b75f
Brady Six <bsixdev@gmail.com>
2024-10-25 17:51:01 -0700

    chore: setup proxy; setup ts path resolution

--- ui/vite.config.ts
+++ ui/vite.config.ts
@@ -1,9 +1,20 @@
+import path from 'node:path'
 import react from '@vitejs/plugin-react'
 import { defineConfig } from 'vitest/config'
 
 export default defineConfig({
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, 'src'),
+    },
+  },
   plugins: [react()],
   test: {
     environment: 'jsdom',
+  },
+  server: {
+    proxy: {
+      '/api': 'http://localhost:8080',
+    },
   },
 })
