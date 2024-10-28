5fb9720506085bf06d3b08b11e46a7f5a8561818
Brady Six <bsixdev@gmail.com>
2024-10-27 19:37:37 -0700

    style(api): lint fix

--- simple-contact-api/src/test/java/com/bsix/sca/AppTest.java
+++ simple-contact-api/src/test/java/com/bsix/sca/AppTest.java
@@ -1,12 +1,11 @@
 package com.bsix.sca;
 
-
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 public class AppTest {
 
-    @Test
-    public void contextLoads() {}
+  @Test
+  public void contextLoads() {}
 }
