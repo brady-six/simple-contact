b75adc1a0c542541fe03c5764b18463236c55aa9
Brady Six <bsixdev@gmail.com>
2024-10-29 15:52:39 -0700

    fix(api): only seed database while testing

--- simple-contact-api/src/main/java/com/bsix/sca/Seed.java
+++ simple-contact-api/src/main/java/com/bsix/sca/Seed.java
@@ -5,8 +5,10 @@ import com.bsix.sca.contact.ContactRepository;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 
-@Configuration
+@Configuration()
+@Profile("test")
 public class Seed {
 
   @Bean
--- simple-contact-api/src/test/java/com/bsix/sca/AppTest.java
+++ simple-contact-api/src/test/java/com/bsix/sca/AppTest.java
@@ -2,8 +2,10 @@ package com.bsix.sca;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles(profiles = "test")
 public class AppTest {
 
   @Test
