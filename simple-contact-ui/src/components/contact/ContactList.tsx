f204c20463088c3c59859c9059d56a3652d2858a
Brady Six <bsixdev@gmail.com>
2024-10-29 13:06:38 -0700

    style(ui): add gap and horizontal margin to contact list component

--- simple-contact-ui/src/components/contact/ContactList.tsx
+++ simple-contact-ui/src/components/contact/ContactList.tsx
@@ -5,7 +5,7 @@ export default function ContactList() {
   const { pageData } = useContact()
 
   return (
-    <ul className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4">
+    <ul className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-5 mx-16">
       {
         pageData._embedded?.contactList.map(c => <ContactCard key={c.id} contact={c} />)
       }
