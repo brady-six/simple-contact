bd6a76bcdace5a17bd8b33aa0f99a328578f6502
Brady Six <bsixdev@gmail.com>
2024-10-28 22:28:39 -0700

    feat(ui): define contact data models

---
+++ simple-contact-ui/src/model.ts
@@ -0,0 +1,48 @@
+export interface ContactListPageMeta {
+  size: number
+  totalElements: number
+  totalPages: number
+}
+
+export interface ContactListLinkMeta {
+  self: {
+    href: string
+  }
+  prev?: {
+    href: string
+  }
+  next?: {
+    href: string
+  }
+}
+
+export interface ContactEntity {
+  id: string
+  firstName: string
+  lastName: string
+  phoneNumber: string
+  profilePicture: string
+  address: {
+    street: string
+    city: string
+    state: string
+    zipcode: string
+    country: string
+  }
+  _links: {
+    self: {
+      href: string
+    }
+    collection: {
+      href: string
+    }
+  }
+}
+
+export interface ContactPage {
+  _embedded?: {
+    contactList: ContactEntity[]
+  }
+  _links: ContactListLinkMeta
+  page: ContactListPageMeta
+}
