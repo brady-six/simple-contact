4aeb9dcae9f995a347bbf6b81fc1ba9e36e5110b
Brady Six <bsixdev@gmail.com>
2024-10-29 13:11:50 -0700

    refactor(ui): define root layout

--- simple-contact-ui/src/App.tsx
+++ simple-contact-ui/src/App.tsx
@@ -1,5 +1,14 @@
+import ContactCreate from './components/contact/ContactCreate'
+import ContactList from './components/contact/ContactList'
+import ContactPaginate from './components/contact/ContactPaginate'
+import ContactProvider from './components/contact/ContactProvider'
+
 export default function App() {
   return (
-    <div></div>
+    <ContactProvider>
+      <ContactPaginate />
+      <ContactCreate />
+      <ContactList />
+    </ContactProvider>
   )
 }
